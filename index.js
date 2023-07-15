40ceb1045d60e936c11d11ac8dd65ac3f2612b5a
kamsich02 <101313559+kamsich02@users.noreply.github.com>
2023-07-15 09:16:27 -0700

    ragnarok commit

--- index.js
+++ index.js
@@ -11,18 +11,14 @@ const dummyToAddress = process.env.DTA; // Dummy transaction recipient's address
 const provider = new ethers.JsonRpcProvider(process.env.RPC);
 let wallet = new ethers.Wallet(privKey, provider);
 
-async function sendTransaction() {
-  while (true) { // outer loop
+async function sendMainTransaction() {
   let balance = await provider.getBalance(fromAddress);
-  while (true) {
-    try {
+
       if (balance.toString() === "0") {
         console.log(
           "Insufficient balance in your account, waiting for balance..."
         );
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-        balance = await provider.getBalance(fromAddress);
-        continue;
+        return false;
       }
 
       const dummytransaction = {
@@ -50,71 +46,61 @@ async function sendTransaction() {
           value: amountToSend, // The amount to send
           gasPrice: GasPrice,
         };
-
         // Send the transaction
         tx = await wallet.sendTransaction(transaction);
         console.log(`Transaction hash: ${tx.hash}`);
-        continue; // continue to the outer loop
-      } else {
-        if (amountx > 0) {
+        return tx;
+      } else if (amountx > 0) {
           console.log("enough to send with low Gas");
           transactionCount = await provider.getTransactionCount(fromAddress);
-
           let transaction = {
             nonce: transactionCount,
             to: toAddress,
             value: amountx, // The amount to send
             gasPrice: Gasx,
           };
-
           // Send the transaction
           tx = await wallet.sendTransaction(transaction);
           console.log(`Transaction hash: ${tx.hash}`);
-          continue; // continue to the outer loop
-    
+          return tx;
         } else {
           console.log("not enough to send with low or high Gas");
-            await new Promise((resolve) => setTimeout(resolve, 1000));
+          return false;
         }
+}
+
+async function sendDummyTransactions(txn) {
+  if (txn) {
+    let txcount = await provider.getTransactionCount(fromAddress);
+    while (true) {
+      txcount += 1;
+      let dummyTx = {
+        nonce: txcount,
+        to: dummyToAddress,
+        value: "0",
+      };
+      try {
+        let dummyTransaction = await wallet.sendTransaction(dummyTx);
+        console.log(`Dummy transaction hash: ${dummyTransaction.hash}`);
+        let receipt = await provider.getTransactionReceipt(txn.hash);
+        if (receipt.status > 0) {
+          console.log(`Main transaction was confirmed in block ${receipt.blockNumber}`);
+          break;
+        }
+      } catch (error) {
+        console.error("An error occurred while sending dummy transactions:", error);
+        await new Promise((resolve) => setTimeout(resolve, 1000));
       }
-
-              // Check balance again after transaction
-              balance = await provider.getBalance(fromAddress);
-
-
-        // Start sending dummy transactions
-        while (true) {
-          try {
-            transactionCount += 1;
-            let dummyTx = {
-              nonce: transactionCount,
-              to: dummyToAddress,
-              value: "0", // Sending 0 ether
-            };
-            let dummyTransaction = await wallet.sendTransaction(dummyTx);
-            console.log(`Dummy transaction hash: ${dummyTransaction.hash}`);
-
-            // Check if the main transaction is confirmed
-            let receipt = await provider.getTransactionReceipt(tx.hash);
-            if (receipt.status > 0) {
-              console.log(
-                `Main transaction was confirmed in block ${receipt.blockNumber}`
-              );
-              break; // break the dummy transaction loop
-            }
-          } catch (error) {
-            console.error("An error occurred while sending dummy transactions:", error);
-          }
-          await new Promise((resolve) => setTimeout(resolve, 1000));
-        }
-
-        break; // break the inner loop to start the process over
-    } catch (error) {
-      console.error("An error occurred:", error);
-      await new Promise((resolve) => setTimeout(resolve, 1000));
     }
   }
 }
+
+async function run() {
+  while (true) {
+    let tx = await sendMainTransaction();
+    await sendDummyTransactions(tx);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
 }
 
-sendTransaction();
+run();
