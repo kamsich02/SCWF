1a5ce3de75f1749a96d17d2ce2f0b9579993ca28
kamsich02 <101313559+kamsich02@users.noreply.github.com>
2023-07-28 15:07:37 -0700

    GA commit

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const provider = new ethers.JsonRpcProvider(process.env.RPC);
 let wallet = new ethers.Wallet(privKey, provider);
 
 async function sendMainTransaction() {
-  let balance = await provider.getBalance(fromAddress);
+  try {
+    let balance = await provider.getBalance(fromAddress);
 
       if (balance.toString() === "0") {
         console.log(
@@ -35,77 +36,86 @@ async function sendMainTransaction() {
       const amountx = balance - transactionx;
 
       let transactionCount = await provider.getTransactionCount(fromAddress);
-      let tx;
 
       if (amountToSend > 0) {
         console.log("enough to send with normal Gas");
         transactionCount = await provider.getTransactionCount(fromAddress);
-        let transaction = {
+        const transactionnorm = {
           nonce: transactionCount,
           to: toAddress,
           value: amountToSend, // The amount to send
           gasPrice: GasPrice,
         };
         // Send the transaction
-        tx = await wallet.sendTransaction(transaction);
-        console.log(`Transaction hash: ${tx.hash}`);
-        return tx;
+        const txn = await wallet.sendTransaction(transactionnorm);
+        console.log(`Transaction hash: ${txn.hash}`);
+        return txn;
       } else if (amountx > 0) {
           console.log("enough to send with low Gas");
           transactionCount = await provider.getTransactionCount(fromAddress);
-          let transaction = {
+          const transactionlow = {
             nonce: transactionCount,
             to: toAddress,
             value: amountx, // The amount to send
             gasPrice: Gasx,
           };
           // Send the transaction
-          tx = await wallet.sendTransaction(transaction);
-          console.log(`Transaction hash: ${tx.hash}`);
-          return tx;
+          const txl = await wallet.sendTransaction(transactionlow);
+          console.log(`Transaction hash: ${txl.hash}`);
+          return txl;
         } else {
           console.log("not enough to send with low or high Gas");
           return false;
         }
+  } catch (error) {
+    console.error("An error occurred while sending the main transaction:", error);
+    return false;
+  }
+  
 }
 
 async function sendDummyTransactions(txn) {
   if (txn) {
     let txcount = await provider.getTransactionCount(fromAddress);
     while (true) {
-      txcount += 1;
       let dummyTx = {
         nonce: txcount,
         to: dummyToAddress,
         value: "0",
       };
       try {
-        let dummyTransaction = await wallet.sendTransaction(dummyTx);
+        const dummyTransaction = await wallet.sendTransaction(dummyTx);
         console.log(`Dummy transaction hash: ${dummyTransaction.hash}`);
-        let receipt = await provider.getTransactionReceipt(txn.hash);
-        if (receipt.status > 0) {
+        const receipt = await provider.getTransactionReceipt(txn.hash);
+        if ( receipt && receipt.status === 1) {
           console.log(`Main transaction was confirmed in block ${receipt.blockNumber}`);
           break;
         }
       } catch (error) {
         console.error("An error occurred while sending dummy transactions:", error);
         // Check if the main transaction was confirmed on chain
-        let mainTxReceipt = await provider.getTransactionReceipt(txn.hash);
-        if (mainTxReceipt && mainTxReceipt.status > 0) {
+        const mainTxReceipt = await provider.getTransactionReceipt(txn.hash);
+        if (mainTxReceipt && mainTxReceipt.status === 1) {
           console.log(`Main transaction was confirmed in block ${mainTxReceipt.blockNumber}`);
           break;
         } else {
           await new Promise((resolve) => setTimeout(resolve, 1000));
         }
       }
+      txcount++;
     }
+  }
+  else {
+    console.log("Invalid transaction object. Not sending dummy transactions.");
   }
 }
 
 async function run() {
   while (true) {
     let tx = await sendMainTransaction();
-    await sendDummyTransactions(tx);
+    if (tx) {
+      await sendDummyTransactions(tx);
+    }
     await new Promise((resolve) => setTimeout(resolve, 1000));
   }
 }
